1aa1cae1f97895ef775349a4acec860bd2322d95
stenlyadam <stenly.adam@unklab.ac.id>
2024-03-07 15:56:01 +0800

    adding react navigation, sign in page, refactor router and pages

--- App.tsx
+++ App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import SplashScreen from './src/pages/SplashScreen';
+import {NavigationContainer} from '@react-navigation/native';
+import Router from './src/router';
 
 const App = () => {
-  return <SplashScreen />;
+  return (
+    <NavigationContainer>
+      <Router />
+    </NavigationContainer>
+  );
 };
 
 export default App;
--- src/pages/SplashScreen/index.tsx
+++ src/pages/SplashScreen/index.tsx
@@ -1,8 +1,14 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useEffect} from 'react';
 import Logo from '../../assets/icon/Logo.svg';
 
-const SplashScreen = () => {
+const SplashScreen = ({navigation}) => {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.replace('SignIn');
+    }, 3000);
+  }, []);
+
   return (
     <View style={styles.container}>
       <Logo />
