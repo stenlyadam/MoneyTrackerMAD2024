ccc4f2a8a35e68306aadad124fc08f043fe414d2
stenlyadam <stenly.adam@unklab.ac.id>
2024-05-07 03:05:43 +0800

    Demo: 5. Home page firebase integration (Read onValue) 6. Add Transaction page (Push list of data) 7. Add Transaction page (Read and Query list of data)

--- src/pages/AddTransaction/index.tsx
+++ src/pages/AddTransaction/index.tsx
@@ -1,5 +1,5 @@
 import {StyleSheet, Text, View, ScrollView} from 'react-native';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   Button,
   Gap,
@@ -7,9 +7,107 @@ import {
   TextInput,
   TransactionCard,
 } from '../../components';
+import moment from 'moment';
+import {
+  getDatabase,
+  ref,
+  push,
+  set,
+  get,
+  onValue,
+  query,
+  limitToLast,
+  orderByChild,
+  equalTo,
+} from 'firebase/database';
+import {showMessage} from 'react-native-flash-message';
 
 const AddTransaction = ({navigation, route}) => {
-  const {title} = route.params;
+  const {title, uid} = route.params;
+  const [description, setDescription] = useState('');
+  const [type, setType] = useState('');
+  const [amount, setAmount] = useState('');
+  const [transactions, setTransactions] = useState([]);
+
+  const db = getDatabase();
+  useEffect(() => {
+    const transactionsRef = query(
+      ref(db, `transactions/${uid}`),
+      orderByChild('category'),
+      equalTo(title),
+      limitToLast(3),
+    );
+    onValue(transactionsRef, snapshot => {
+      if (snapshot.exists()) {
+        const data = snapshot.val();
+        const transactionsArray = [];
+        Object.keys(data).map(key => {
+          transactionsArray.push(data[key]);
+        });
+        setTransactions(transactionsArray.reverse());
+      }
+    });
+  }, []);
+
+  const onSubmit = () => {
+    const data = {
+      dateTime: moment().format('LLL'),
+      description: description,
+      type: type,
+      amount: amount,
+      category: title,
+    };
+
+    const transationRef = ref(db, `transactions/${uid}`);
+    const newTransactionRef = push(transationRef);
+    set(newTransactionRef, data);
+
+    //Update balance
+    const userBalanceRef = ref(db, `users/${uid}/balance`);
+    get(userBalanceRef)
+      .then(snapshot => {
+        if (snapshot.exists()) {
+          let {total, cashOnBank, cashOnHand} = snapshot.val();
+          if (type === 'Debit') {
+            if (title === 'Cash On Bank') {
+              cashOnBank = cashOnBank + Number(amount);
+            } else {
+              cashOnHand = cashOnHand + Number(amount);
+            }
+            total = total + Number(amount);
+          } else {
+            if (title === 'Cash On Bank') {
+              cashOnBank = cashOnBank - Number(amount);
+            } else {
+              cashOnHand = cashOnHand - Number(amount);
+            }
+            total = total - Number(amount);
+          }
+          const newBalance = {
+            total: total,
+            cashOnBank: cashOnBank,
+            cashOnHand: cashOnHand,
+          };
+          const userRef = ref(db, `users/${uid}/balance`);
+          set(userRef, newBalance);
+          //Clear text input
+          setDescription('');
+          setType('');
+          setAmount('');
+
+          showMessage({
+            message: 'Transaksi berhasil ditambahkan',
+            type: 'success',
+          });
+        } else {
+          console.log('No data available');
+        }
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
   return (
     <ScrollView
       style={styles.pageContainer}
@@ -20,31 +118,38 @@ const AddTransaction = ({navigation, route}) => {
         onPress={() => navigation.goBack()}
       />
       <View style={styles.contentWrapper}>
-        <TextInput label="Description" placeholder="Add the description" />
+        <TextInput
+          label="Description"
+          placeholder="Add the description"
+          value={description}
+          onChangeText={value => setDescription(value)}
+        />
         <Gap height={17} />
-        <TextInput label="Type" placeholder="Debit / Kredit" />
+        <TextInput
+          label="Type"
+          placeholder="Debit / Kredit"
+          value={type}
+          onChangeText={value => setType(value)}
+        />
         <Gap height={17} />
-        <Button label="Save" />
+        <TextInput
+          label="Amount"
+          placeholder="Amount"
+          value={amount}
+          onChangeText={value => setAmount(value)}
+        />
+        <Gap height={17} />
+        <Button label="Save" onPress={onSubmit} />
         <Gap height={17} />
         <Text style={styles.subTitle}>Last 3 Transactions</Text>
-        <TransactionCard
-          date="17 April 2024"
-          desc="Water, Food"
-          price="-Rp. 400.000"
-          type="debit"
-        />
-        <TransactionCard
-          date="17 April 2024"
-          desc="Office supplies"
-          price="-Rp. 400.000"
-          type="debit"
-        />
-        <TransactionCard
-          date="17 April 2024"
-          desc="Top Up"
-          price="Rp. 400.000"
-          type="credit"
-        />
+        {transactions.map(item => (
+          <TransactionCard
+            date={item.dateTime}
+            desc={item.description}
+            price={item.amount}
+            type={item.type}
+          />
+        ))}
       </View>
     </ScrollView>
   );
