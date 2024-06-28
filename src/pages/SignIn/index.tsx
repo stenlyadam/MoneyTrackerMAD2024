c8860f795320b89b8f688616fe154c8e99bc8d0d
stenlyadam <stenly.adam@unklab.ac.id>
2024-06-28 14:48:07 +0800

    sign in

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, View} from 'react-native';
 import {Button, Gap} from '../../components/atoms';
 import {Header, TextInput} from '../../components/molecules';
+import {getAuth, signInWithEmailAndPassword} from 'firebase/auth';
+import {showMessage} from 'react-native-flash-message';
 
 const SignIn = ({navigation}) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const auth = getAuth();
+  const onSubmit = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then(userCredential => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+        navigation.navigate('HomeScreen', {uid: user.uid});
+      })
+      .catch(error => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        showMessage({
+          message: errorMessage,
+          type: 'danger',
+        });
+      });
+  };
+
   return (
     <View style={styles.pageContainer}>
       <Header title="Sign In" />
@@ -12,14 +35,17 @@ const SignIn = ({navigation}) => {
         <TextInput
           label="Email Address"
           placeholder="Type your email address"
+          onChangeText={value => setEmail(value)}
         />
         <Gap height={16} />
-        <TextInput label="Password" placeholder="Type your password" />
-        <Gap height={24} />
-        <Button
-          label="Sign In"
-          onPress={() => navigation.navigate('HomeScreen')}
+        <TextInput
+          label="Password"
+          placeholder="Type your password"
+          onChangeText={value => setPassword(value)}
+          secureTextEntry={true}
         />
+        <Gap height={24} />
+        <Button label="Sign In" onPress={onSubmit} />
         <Gap height={12} />
         <Button
           label="Create New Account"
